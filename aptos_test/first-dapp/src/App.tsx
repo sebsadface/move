c92ea4e9aeeefbf1a8aabb97acf2f1af6394d0c7
Sebastian Liu <sebsadface@gmail.com>
2022-12-17 15:22:23 -0800

    hello_blockchain module created

--- aptos_test/first-dapp/src/App.tsx
+++ aptos_test/first-dapp/src/App.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Types, AptosClient } from 'aptos';
+
+// Create an AptosClient to interact with devnet.
+const client = new AptosClient('https://fullnode.devnet.aptoslabs.com/v1');
+
 
 function App() {
+  // Retrieve aptos.account on initial render and store it.
+  const [address, setAddress] = React.useState<string | null>(null);
+  
+  /**
+   * init function
+   */
+  const init = async() => {
+    // connect
+    const { address, publicKey } = await window.aptos.connect();
+    setAddress(address);
+  }
+  
+  React.useEffect(() => {
+     init();
+  }, []);
+
+  // Use the AptosClient to retrieve details about the account.
+  const [account, setAccount] = React.useState<Types.AccountData | null>(null);
+  React.useEffect(() => {
+    if (!address) return;
+    client.getAccount(address).then(setAccount);
+  }, [address]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p><code>{ address }</code></p>
+      <p><code>{ account?.sequence_number }</code></p>
     </div>
   );
 }
